2f7d924f620a708a6fddb380e75330fad3301062
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-02 22:49:06 +0200

    feat(chiamata agli epi con axios ,salvo i dati e mostro dati aggiornati)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  
+  return(
+  <div className="container py-5">
+    <h1 className='text-center mb-5'>Lista Attori</h1>
+    <div className="row g-4">
+      <div className="col-12 col md-6 col-lg-4">
+        <img src="" alt="" />
+        <div className="card-body">
+          <h5 className="card title">Nome Attore</h5>
+          <p className='card-text'><strong>Anno di nascita:</strong></p>
+           <p className='card-text'><strong>Nazionalità:</strong></p>
+            <p className='card-text'><strong>Biogafia:</strong></p>
+             <p className='card-text'><strong>Riconoscimenti</strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default App
